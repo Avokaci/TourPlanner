48d4cd7d154b64e0729178703d5c950c1514375d
Avokaci <if19b123@technikum-wien.at>
2021-04-13 18:38:43 +0200

    Split into model, view, viewmodel and completed view from add tour

---
+++ TourPlanner.BusinessLayer/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace TourPlanner.BusinessLayer
+{
+    public class Class1
+    {
+    }
+}
---
+++ TourPlanner.DataAccessLayer/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace TourPlanner.DataAccessLayer
+{
+    public class Class1
+    {
+    }
+}
